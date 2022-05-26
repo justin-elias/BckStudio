18c639a60332c1cfba4ea94f087fa1739955415b
stryder03 <stryder03@users.noreply.github.com>
2022-05-26 11:13:39 -0600

    chore(components): added MainLogo

--- src/components/Layout/LayoutSections/MainLogo.tsx
+++ src/components/Layout/LayoutSections/MainLogo.tsx
@@ -4,21 +4,36 @@ import React from "react";
 import {makeStyles} from "@mui/styles";
 //@ts-ignore
 import classNames from "classnames";
+import {mainLogoStyle} from "../../../assets/globals";
 //Components
+import { Hidden } from "@mui/material";
+import GridItem from "../../Grid/GridItem";
+import GridContainer from "../../Grid/GridContainer";
+import Image from "next/image";
 // Assets
 
-/* eslint-disable no-unused-vars */
+// @ts-ignore
 const useStyles = makeStyles(() => ({
-    /* eslint-enable no-unused-vars */
+    ...mainLogoStyle,
+    heroButton: {
+        position: "sticky",
+    }
 
 }));
 
-export default function MainLogo(props: any) {
+export default function MainLogo() {
     const classes = useStyles();
-    const {} = props
     return (
         <React.Fragment>
-
+            <div className={classes.container}>
+                <GridContainer>
+                    <GridItem xs={12} sm={12} md={6}>
+                        <Hidden smDown>
+                            <Image src={"https://media.graphassets.com/XMAKIRSSVyM1QPf2Y8hG"} alt={"Bozeman Community Kiln"} height={"197"} width={"704"} unoptimized/>
+                        </Hidden>
+                    </GridItem>
+                </GridContainer>
+            </div>
         </React.Fragment>
     );
 }
